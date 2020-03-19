7a2801689cd6d299ff7eeaf852321caab207d8bb
inbarshm <58911537+inbarshm@users.noreply.github.com>
2020-03-19 18:52:02 +0100

    Ex_5 Attempt

--- Inbar/Ex_5_r.R
+++ Inbar/Ex_5_r.R
@@ -3,18 +3,17 @@ library(grf)
 
 df <- read.csv("~/Dropbox/Msc/Econ & ML/Ex_5.csv")
 
-X <- df %>% select(X0, X1, X2, X3, X4, D) %>% as.matrix()
+X <- df %>% select(x1, x2, x3, x4, x5, D) %>% as.matrix()
 
-Y <- df %>% select(Y1) %>% as.matrix()
+Y <- df %>% select(y) %>% as.matrix()
 
-W <- df %>% select(Tau) %>% as.matrix()
+W <- df %>% select(T) %>% as.matrix()
 
 # Train a causal forest.
-tcf <- causal_forest(X, Y, W)
+cf <- causal_forest(X, Y, W)
 
 # Estimate treatment effects for the training data using out-of-bag prediction.
-tau <- predict(tcf)
+tau <- predict(cf, X)
 #hist(tau.hat.oob$predictions)
 
-
-plot(df$Tau, tau$predictions)
+write.csv(tau, "individual_treatment_effects.csv")
