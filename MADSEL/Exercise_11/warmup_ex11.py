74be6809de0367f0db80dc10bb37837c325fa22b
madselarsen <bfj396@alumni.ku.dk>
2020-05-08 16:27:16 +0200

    New warmup

--- MADSEL/Exercise_11/warmup_ex11.py
+++ MADSEL/Exercise_11/warmup_ex11.py
@@ -1,19 +1,20 @@
-import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
-X = [1]
 
 def TnFactory(n):
-    for x in X:
-        if n == 0:
+    if n == 0:
+        def T(x):
             return 1
-        elif n == 1:
+    elif n == 1:
+        def T(x):
             return x
-        else:
-            return 2*TnFactory(n-1) - TnFactory(n-2)
+    else:
+        def T(x):
+            return 2*x*TnFactory(n-1)(x) - TnFactory(n-2)(x)
+    return T
+
+N = 5
+for i in range(1,N+1):
+    plt.plot(x, TnFactory(i)(x))
 
 
-
-TnFactory(5)
-
-#This does not work
